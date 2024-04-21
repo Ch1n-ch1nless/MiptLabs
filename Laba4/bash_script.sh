7a5483e990256a5812b711307a247e51a912f4d8
Ch1n-ch1nless <aleksandrov.oa@phystech.edu>
2024-04-22 01:53:31 +0300

    Update README.md/write about O(NlogN)

--- Laba4/bash_script.sh
+++ Laba4/bash_script.sh
@@ -9,9 +9,15 @@ touch time_file.csv
 
 gcc create_random_array.cpp -o create_random_array.out
 
+make FLAG = -DLINEAR_BUILD
+
 for (( i=$from; i <= $to; i += $step))
 do
 touch "Tests/${i}.csv"
 ./create_random_array.out $i $max_value "Tests/${i}.csv"
 ./binary_heap.out $i "Tests/${i}.csv" "time_file.csv"
 done
+
+make clean
+rm create_random_array.out
+rm binary_heap.out
